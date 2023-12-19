b5bbe16d52fb3437c02bd67cb1460073d167d1b4
Chiranjeevi315 <72541800+Chiranjeevi315@users.noreply.github.com>
2023-12-19 14:27:55 +0530

    Update main.tf
    updated

--- Lab-Create-Resource-Group/main.tf
+++ Lab-Create-Resource-Group/main.tf
@@ -14,10 +14,6 @@ terraform {
 # our Azure account
 
 provider "azurerm" {
-    subscription_id = "0b03cd52-5058-421a-9555-bdd3371ab590"
-    client_id = "dafc302c-d0e3-4a7d-bdf4-505f23f7a400"
-    client_secret = "Cu.8Q~afkQFKT1uekfI3I0ImcMr64VCjOhmYUdgr"
-    tenant_id = "b0ac77ef-0577-4199-9cf2-036f07fedcfa" 
     features {}
 }
 
