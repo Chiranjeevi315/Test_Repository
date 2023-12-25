9ba3c099bdacb12d97d820889a9017bdff3d34d4
Chiranjeevi315 <72541800+Chiranjeevi315@users.noreply.github.com>
2023-12-25 17:24:56 +0530

    Update main.tf
    updated main file

--- Lab-Create-Resource-Group/main.tf
+++ Lab-Create-Resource-Group/main.tf
@@ -1,26 +1,106 @@
-# We first specify the terraform provider. 
-# Terraform will use the provider to ensure that we can work with Microsoft Azure
-
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.92.0"
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
   }
 }
 
-# Here we need to mention the Azure AD Application Object credentials to allow us to work with 
-# our Azure account
-
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    features {}
+  features {}  
 }
 
-# The resource block defines the type of resource we want to work with
-# The name and location are arguements for the resource block
+terraform {
+  backend "azurerm" {
+    storage_account_name = "__terraformstorageaccount__"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
 
-resource "azurerm_resource_group" "my_grp"{
-  name="rgrp-tf" 
-  location="North Europe"
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "__storagekey__"
+    features {}
+  }
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.rgname}"
+  location = "${var.rglocation}"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "${var.prefix}-10"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rg.location}"
+  address_space       = ["${var.vnet_cidr_prefix}"]
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  address_prefixes     = ["${var.subnet1_cidr_prefix}"]
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "${var.prefix}-nsg1"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rg.location}"
+}
+
+# NOTE: this allows RDP from any network
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "rdp"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
+  network_security_group_name = "${azurerm_network_security_group.nsg1.name}"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet_assoc" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                            = "${var.prefix}-vmt01"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  network_interface_ids = [ azurerm_network_interface.nic1.id ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2012-R2-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
 }
